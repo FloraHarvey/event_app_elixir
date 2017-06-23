a874452e84fdfb031dd745d8d28c00867d78f429
Flora Harvey <fharvey555@gmail.com>
2017-06-23 15:56:55 +0100

    Add channel to update available spaces in real time

--- apps/rsvp_web/web/static/js/event.js
+++ apps/rsvp_web/web/static/js/event.js
@@ -1,5 +1,20 @@
 import socket from './socket'
 
-// (function(){
-//   le id = $('#id').data('id');
-// })
+(function(){
+  let id = $('#id').data('id');   //fetch event id
+  if (!id)
+    return;
+
+  let channel = socket.channel("event:" + id, {});    // create channel with event:id as title
+
+  channel.on("update_quantity", payload => {
+    console.log("Update", payload);
+    $('h4 span').text(payload.quantity);
+  })
+
+  channel.join()
+    .receive("ok", resp => { console.log("Joined successfully event:" + id, resp)
+    })
+    .receive("error", resp => { console.log("Unable to join", resp)
+    })
+})();
